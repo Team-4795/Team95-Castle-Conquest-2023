04c6f0ad6be6e253b776f82312bee4bc14319b2b
GooseJuice898 <oscarburgess262@gmail.com>
2023-11-10 10:03:22 -0500

    modified:   build.gradle 	new file:   networktables.json 	new file:   simgui-ds.json 	new file:   simgui.json 	modified:   src/main/java/frc/robot/Constants.java 	new file:   src/main/java/frc/robot/subsystems/Intake.java

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Intake extends SubsystemBase{
+
+    //This makes a motor
+    private final PWMTalonSRX glizzieguzzlerintake = new PWMTalonSRX(0);
+
+    public Intake() {
+        
+    }
+
+    //intakes balls
+    public void makeItSpinIn() {
+        glizzieguzzlerintake.set(0.3);
+    }
+    //outputs balls
+    public void makeItSpinOut() {
+        glizzieguzzlerintake.set(-0.3);
+    }
+}
